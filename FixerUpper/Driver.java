3e2cb52b4cb18f120f6190dda3eb9555f0220ff1
jasonaba <abankwahjason@gmail.com>
2026-09-16 14:06:53 -0400

    Add files via upload

---
+++ FixerUpper/Driver.java
@@ -0,0 +1,11 @@
+package FixerUpper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String expression = "(3*4+(5+6*7))/(8-9)";
+		Fixer Bob = new Fixer(expression);
+	}
+
+}
--- FixerUpper/Fixer.java
+++ FixerUpper/Fixer.java
@@ -5,42 +5,114 @@ import java.util.Stack;
 
 public class Fixer {
 	ArrayList<Character> postfix, prefix;
-	Stack<Character> post_stack, pre_stack;
-	
+
+	// initialize this and then convert it to postfix
 	public Fixer(String infix) {
+		Stack<Character> post_stack, pre_stack;
+		postfix = new ArrayList<Character>();
+		prefix = new ArrayList<Character>();
 		post_stack = new Stack<Character>();
-		pre_stack = new Stack<Character>();
-		for(int i = 0; i<infix.length(); i++) {
-			postfix.set(i, infix.charAt(i));
-			prefix.set(i, infix.charAt(i));
+		boolean closed_parenthesis_found = false;
+		// pre_stack = new Stack<Character>();
+		do {
+			for (char c : infix.toCharArray()) {
+
+				if (c == '(') {
+					post_stack.push(c);
+					System.out.println("Stack");
+					System.out.println(post_stack);
+					System.out.println("ArrayList");
+					System.out.println(postfix);
+				}
+
+				if (isOperator(c)) {
+						while (!post_stack.isEmpty()&&isOperator(post_stack.peek())) {
+							if (getOperatorValue(c) < getOperatorValue(post_stack.peek())) {
+								postfix.add(post_stack.pop());
+							} else {
+								break;
+							}
+						}
+					
+					post_stack.push(c);
+					System.out.println("Stack");
+					System.out.println(post_stack);
+					System.out.println("ArrayList");
+					System.out.println(postfix);
+				}
+
+				if (isOperand(c)) {
+					postfix.add(c);
+					System.out.println("Stack");
+					System.out.println(post_stack);
+					System.out.println("ArrayList");
+					System.out.println(postfix);
+				}
+				if (c == ')') {
+					closed_parenthesis_found = true;
+				}
+				if (!post_stack.isEmpty()&&closed_parenthesis_found) {
+					if (isOperator(c))
+						postfix.add(post_stack.pop());
+					System.out.println("Stack");
+					System.out.println(post_stack);
+					System.out.println("ArrayList");
+					System.out.println(postfix);
+					// else return error message?
+				}
+			}
+		} while (!postfix.isEmpty());
+
+		if (!post_stack.isEmpty()) {
+			System.out.println("error within expression");
 		}
-		evaluatePost(true);
-		evaluatePre(true);
+
+		// evaluatePost(true);
+		// evaluatePre(true);
 	}
-	
+
 	/**
+	 * Evaluates the value using the postfix (evaluate first, and if there is an
+	 * error, then don't allow the user to evaluate any longer)
 	 * 
 	 * @param showDemo (if the process will be output in the console)
 	 * @return The solved value
 	 */
 	public int evaluatePost(boolean showDemo) {
-		Stack<Character> temp_op = new Stack<Character>();
-		String operators = "(*/+-";
-		for(int i = 0; i<postfix.size(); i++) {
-			if(operators.indexOf(postfix.get(i))>-1) {
-				if(postfix.get(i)=='(') {//Add this to the stack
-					post_stack.push(postfix.get(i));
-				}
-					
-			}
-		}
+
 	}
+
 	/**
 	 * 
 	 * @param showDemo (if the process will be output in the console)
 	 * @return The solved value
 	 */
 	public int evaluatePre(boolean showDemo) {
-		
+
 	}
+
+	private boolean isOperand(char c) {
+		String operands = "0123456789";
+		if (operands.indexOf(c) > -1)
+			return true;
+		return false;
+	}
+
+	private boolean isOperator(char c) {
+		String operators = "+-*/";
+		if (operators.indexOf(c) > -1)
+			return true;
+		return false;
+	}
+
+	private int getOperatorValue(char c) {
+		if (c == '-' || c == '+')
+			return 1;
+		if (c == '/' || c == '*')
+			return 2;
+		if (c == '^')
+			return 3;
+		return 0;
+	}
+
 }
