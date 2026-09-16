a520ed743a0493a1661dedc63f9ba5c21c3af316
jasonaba <abankwahjason@gmail.com>
2026-09-15 20:35:16 -0400

    Add files via upload

--- FixerUpper/Fixer.java
+++ FixerUpper/Fixer.java
@@ -1,12 +1,46 @@
 package FixerUpper;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Fixer {
-	ArrayList<Character> Postfix;
-	ArrayList<Character> Prefix;
+	ArrayList<Character> postfix, prefix;
+	Stack<Character> post_stack, pre_stack;
 	
 	public Fixer(String infix) {
-		Stack<Character> post_stack = new Stack<Character>();
+		post_stack = new Stack<Character>();
+		pre_stack = new Stack<Character>();
+		for(int i = 0; i<infix.length(); i++) {
+			postfix.set(i, infix.charAt(i));
+			prefix.set(i, infix.charAt(i));
+		}
+		evaluatePost(true);
+		evaluatePre(true);
+	}
+	
+	/**
+	 * 
+	 * @param showDemo (if the process will be output in the console)
+	 * @return The solved value
+	 */
+	public int evaluatePost(boolean showDemo) {
+		Stack<Character> temp_op = new Stack<Character>();
+		String operators = "(*/+-";
+		for(int i = 0; i<postfix.size(); i++) {
+			if(operators.indexOf(postfix.get(i))>-1) {
+				if(postfix.get(i)=='(') {//Add this to the stack
+					post_stack.push(postfix.get(i));
+				}
+					
+			}
+		}
+	}
+	/**
+	 * 
+	 * @param showDemo (if the process will be output in the console)
+	 * @return The solved value
+	 */
+	public int evaluatePre(boolean showDemo) {
+		
 	}
 }
