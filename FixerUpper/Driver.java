e0ae2ecf0de2a454a4bf1fb6fc2800f28e210bca
jasonaba <abankwahjason@gmail.com>
2026-09-18 14:06:09 -0400

    Add files via upload

--- FixerUpper/Driver.java
+++ FixerUpper/Driver.java
@@ -4,8 +4,9 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String expression = "(3*4+(5+6*7))/(8-9)";
-		Fixer Bob = new Fixer(expression);
+		String expression1 = "(3*4+(5+6*7))/(8-9)";
+		String expression2 = "3+(4*2)-(6/3)"; 
+		System.out.println(expression2);
+		Fixer Bob = new Fixer(expression2);
 	}
-
 }
--- FixerUpper/Fixer.java
+++ FixerUpper/Fixer.java
@@ -6,71 +6,59 @@ import java.util.Stack;
 public class Fixer {
 	ArrayList<Character> postfix, prefix;
 
-	// initialize this and then convert it to postfix
+	// initialize this Fixer object and then convert it to postfix
 	public Fixer(String infix) {
-		Stack<Character> post_stack, pre_stack;
+		Stack<Character> post_stack;
+		Stack<String> pre_stack;
 		postfix = new ArrayList<Character>();
 		prefix = new ArrayList<Character>();
 		post_stack = new Stack<Character>();
-		boolean closed_parenthesis_found = false;
-		// pre_stack = new Stack<Character>();
-		do {
-			for (char c : infix.toCharArray()) {
+		pre_stack = new Stack<String>();
+		for (char c : infix.toCharArray()) {
 
-				if (c == '(') {
-					post_stack.push(c);
-					System.out.println("Stack");
-					System.out.println(post_stack);
-					System.out.println("ArrayList");
-					System.out.println(postfix);
-				}
-
-				if (isOperator(c)) {
-						while (!post_stack.isEmpty()&&isOperator(post_stack.peek())) {
-							if (getOperatorValue(c) < getOperatorValue(post_stack.peek())) {
-								postfix.add(post_stack.pop());
-							} else {
-								break;
-							}
-						}
-					
-					post_stack.push(c);
-					System.out.println("Stack");
-					System.out.println(post_stack);
-					System.out.println("ArrayList");
-					System.out.println(postfix);
-				}
-
-				if (isOperand(c)) {
-					postfix.add(c);
-					System.out.println("Stack");
-					System.out.println(post_stack);
-					System.out.println("ArrayList");
-					System.out.println(postfix);
-				}
-				if (c == ')') {
-					closed_parenthesis_found = true;
-				}
-				if (!post_stack.isEmpty()&&closed_parenthesis_found) {
-					if (isOperator(c))
-						postfix.add(post_stack.pop());
-					System.out.println("Stack");
-					System.out.println(post_stack);
-					System.out.println("ArrayList");
-					System.out.println(postfix);
-					// else return error message?
-				}
+			if (isOperand(c)) {//add the numbers to the list
+				postfix.add(c);
 			}
-		} while (!postfix.isEmpty());
+			
+			else if (c == '(') {//add the open-parenthesis to the stack 
+				post_stack.push(c);
 
-		if (!post_stack.isEmpty()) {
-			System.out.println("error within expression");
+			}
+
+			else if (isOperator(c)) {
+				while (!post_stack.isEmpty() && isOperator(post_stack.peek())
+						&& getOperatorValue(c) <= getOperatorValue(post_stack.peek())) {//if there is an operator of higher precedence on the stack already
+					postfix.add(post_stack.pop());//add that higher precedence operator to the expression
+				}
+
+				post_stack.push(c);//add the operator to the stack
+			}
+
+			
+			else if (c == ')') {
+				while (!post_stack.isEmpty() && post_stack.peek() != '(') {//while we haven't found an open-parenthesis
+
+					postfix.add(post_stack.pop());//add the operators meant to be inside that parenthesis combo
+				}
+				post_stack.pop();
+			}
 		}
 
+		while (!post_stack.isEmpty()) {//add the rest of the operators on the stack to the expression
+			postfix.add(post_stack.pop());
+		}
+		System.out.println("Post Stack");
+		System.out.println(post_stack);
+		System.out.println("ArrayList");
+		System.out.println(postfix);
+
+		prefix = postfix;
+		
 		// evaluatePost(true);
 		// evaluatePre(true);
 	}
 
+	
 	/**
 	 * Evaluates the value using the postfix (evaluate first, and if there is an
 	 * error, then don't allow the user to evaluate any longer)
@@ -78,18 +66,18 @@ public class Fixer {
 	 * @param showDemo (if the process will be output in the console)
 	 * @return The solved value
 	 */
-	public int evaluatePost(boolean showDemo) {
+	//public int evaluatePost(boolean showDemo) {
 
-	}
+	//}
 
 	/**
 	 * 
 	 * @param showDemo (if the process will be output in the console)
 	 * @return The solved value
 	 */
-	public int evaluatePre(boolean showDemo) {
+	//public int evaluatePre(boolean showDemo) {
 
-	}
+	//}
 
 	private boolean isOperand(char c) {
 		String operands = "0123456789";
@@ -99,7 +87,7 @@ public class Fixer {
 	}
 
 	private boolean isOperator(char c) {
-		String operators = "+-*/";
+		String operators = "+-*/^";
 		if (operators.indexOf(c) > -1)
 			return true;
 		return false;
